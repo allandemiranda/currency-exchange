da993bf4798feecfe779d1274cea1dc45aa4c802
Allan de Miranda Silva <allandemiranda@gmail.com>
2024-07-18 23:53:14 +0200

    init

---
+++ src/main/java/app/bank/dummy/entities/TransactionCreditInfo.java
@@ -0,0 +1,27 @@
+package app.bank.dummy.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import java.io.Serial;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Embeddable
+public class TransactionCreditInfo implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = 4642532559642829109L;
+
+  @NotNull
+  @ManyToOne(optional = false)
+  @JoinColumn(name = "credit_account_id", nullable = false)
+  private Account account;
+
+  @Column(name = "credit_tmp_balance", nullable = false)
+  private double tmpBalance;
+
+}
